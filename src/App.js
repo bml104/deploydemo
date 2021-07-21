81e2610d3de8701e9877f9ff1c79f38b91ed6ae3
Dennis <tpl104@outlook.com>
2021-07-21 11:05:40 -0400

    dev to main

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Dev Build</p>
+        <p>Dev to Main Merge</p>
       </header>
     </div>
   );
